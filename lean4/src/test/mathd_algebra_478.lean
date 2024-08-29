6907f5457772f7b1f0ba97c8bfe659d1c96ea6fd
Andreas Florath <andreas@florath.net>
2024-08-29 18:24:58 +0000

    Adapted mathd_algebra_478
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_algebra_478.lean
+++ lean4/src/test/mathd_algebra_478.lean
@@ -1,7 +1,11 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
+
 theorem mathd_algebra_478
   (b h v : ℝ)
-  (h₀ : 0 < b ∧ 0 < h ∧ 0 < v)
-  (h₁ : v = 1 / 3 * (b * h))
-  (h₂ : b = 30)
-  (h₃ : h = 13 / 2) :
-  v = 65 :=
+  (v0 v1 v2 : ℝ)
+  (hyp0 : 0 < v0 ∧ 0 < v1 ∧ 0 < v2)
+  (hyp1 : v2 = 1 / 3 * (v0 * v1))
+  (hyp2 : v0 = 30)
+  (hyp3 : v1 = 13 / 2) :
+  v2 = 65 := by
