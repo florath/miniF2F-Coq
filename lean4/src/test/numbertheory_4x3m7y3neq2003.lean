6870a2f9dd090012db0df3037340d4e51bcf811c
Andreas Florath <andreas@florath.net>
2024-08-30 06:49:29 +0000

    Prepared numbertheory_4x3m7y3neq2003
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/numbertheory_4x3m7y3neq2003.lean
+++ lean4/src/test/numbertheory_4x3m7y3neq2003.lean
@@ -1,7 +1,6 @@
+import Mathlib.Data.Int.Defs
 import Mathlib.Tactic
 
-XXX Z
-
 theorem numbertheory_4x3m7y3neq2003
-  (x y : ℤ) :
-  4 * x^3 - 7 * y^3 ≠ 2003 :=
+  (v0 v1 : ℤ) :
+  4 * v0^3 - 7 * v1^3 ≠ 2003 :=
