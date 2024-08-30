c5070ddff6f8dd9321bb95df5b236c3d9ca8c88f
Andreas Florath <andreas@florath.net>
2024-08-30 10:33:34 +0000

    Added solution for mathd_numbertheory_1124
    Signed-off-by: Andreas Florath <andreas@florath.net>

---
+++ lean4/src/test/mathd_numbertheory_1124-solution.lean
@@ -0,0 +1,13 @@
+import Mathlib.Data.Nat.Defs
+import Mathlib.Tactic
+
+namespace alphalean
+
+-- version b9642c334f09c5f2ed1fc71e05fa9eef718f503e
+-- found 2024-08-30 12:32 during model training
+theorem mathd_numbertheory_1124
+  (v0 : ℕ)
+  (hyp0 : v0 ≤ 9)
+  (hyp1 : 18∣374 * 10 + v0) :
+  v0 = 4 := by
+omega
