e4979db7952d101e00578aa183fdbea9786f0186
Andreas Florath <andreas@florath.net>
2024-08-30 12:05:07 +0000

    Added solution for mathd_algebra_33
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_algebra_33-solution.lean
+++ lean4/src/test/mathd_algebra_33-solution.lean
@@ -3,6 +3,8 @@ import Mathlib.Tactic
 
 namespace alphalean
 
+-- version c37fb28ad027659f765f1e396e365b2e2c6fde4a
+-- found 2024-08-30 14:04 during model training
 theorem mathd_algebra_33
   (v0 v1 v2 : ℝ)
   (hyp0 : v0 ≠ 0)
