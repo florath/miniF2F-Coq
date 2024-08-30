b44ba4c8c2fb8a90c1aa486f72f847e294fece65
Andreas Florath <andreas@florath.net>
2024-08-30 10:17:51 +0000

    Fixed imo_1983_p6 for lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/imo_1983_p6.lean
+++ lean4/src/test/imo_1983_p6.lean
@@ -1,7 +1,10 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
+
 theorem imo_1983_p6
-  (a b c : ℝ)
-  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c)
-  (h₁ : c < a + b)
-  (h₂ : b < a + c)
-  (h₃ : a < b + c) :
-  0 ≤ a^2 * b * (a - b) + b^2 * c * (b - c) + c^2 * a * (c - a) := by
+  (v0 v1 v2 : ℝ)
+  (hyp0 : 0 < v0 ∧ 0 < v1 ∧ 0 < v2)
+  (hyp1 : v2 < v0 + v1)
+  (hyp2 : v1 < v0 + v2)
+  (hyp3 : v0 < v1 + v2) :
+  0 ≤ v0^2 * v1 * (v0 - v1) + v1^2 * v2 * (v1 - v2) + v2^2 * v0 * (v2 - v0) := by
