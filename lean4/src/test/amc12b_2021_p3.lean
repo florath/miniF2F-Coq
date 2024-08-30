6b1458620c70d1f046a1044fedc25a862bed08f6
Andreas Florath <andreas@florath.net>
2024-08-30 11:18:03 +0000

    Prepared amc12b_2021_p3 for lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/amc12b_2021_p3.lean
+++ lean4/src/test/amc12b_2021_p3.lean
@@ -1,5 +1,9 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
+
+namespace alphalean
+
 theorem amc12b_2021_p3
   (x : ℝ)
   (h₀ : 2 + 1 / (1 + 1 / (2 + 2 / (3 + x))) = 144 / 53) :
   x = 3 / 4 := by
-  
