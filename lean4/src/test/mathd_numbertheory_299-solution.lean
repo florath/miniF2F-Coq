81e8da6c3f3c09f93362e0531bc699e0f9ead06f
Andreas Florath <andreas@florath.net>
2024-08-30 12:25:49 +0000

    Add solution for mathd_numbertheory_299
    Signed-off-by: Andreas Florath <andreas@florath.net>

---
+++ lean4/src/test/mathd_numbertheory_299-solution.lean
@@ -0,0 +1,16 @@
+import Mathlib.Data.Nat.Defs
+import Mathlib.Tactic
+
+namespace alphalean
+
+-- version b08bc1809c0283a4b4fd3d96b50e52a3863a97f0
+-- found 2024-08-30 14:23 during model training
+theorem mathd_numbertheory_299 :
+  (1 * 3 * 5 * 7 * 9 * 11 * 13) % 10 = 5 := by
+zify
+
+-- version b08bc1809c0283a4b4fd3d96b50e52a3863a97f0
+-- found 2024-08-30 14:23 during model training
+theorem mathd_numbertheory_299_alt_00 :
+  (1 * 3 * 5 * 7 * 9 * 11 * 13) % 10 = 5 := by
+trivial
