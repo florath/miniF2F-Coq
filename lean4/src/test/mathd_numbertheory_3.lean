c9bde629b1a337eab70297cf45a1bf6a7c353df8
Andreas Florath <andreas@florath.net>
2024-08-30 06:32:45 +0000

    Translated mathd_numbertheory_3 to Lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_numbertheory_3.lean
+++ lean4/src/test/mathd_numbertheory_3.lean
@@ -1,2 +1,6 @@
+import Mathlib.Algebra.BigOperators.Intervals
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Nat.Defs
+
 theorem mathd_numbertheory_3 :
-  (∑ x in finset.range 10, ((x + 1)^2)) % 10 = 5 := by
+  (∑ v0 in Finset.range 10, ((v0 + 1)^2)) % 10 = 5 := by
