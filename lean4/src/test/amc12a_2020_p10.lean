6bd9fcc1c60a3f2dfa86919aa9c8c5aae43192bb
Andreas Florath <andreas@florath.net>
2024-08-30 14:14:51 +0000

    Prepared amc12a_2020_p10 for lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/amc12a_2020_p10.lean
+++ lean4/src/test/amc12a_2020_p10.lean
@@ -1,5 +1,11 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Nat.Defs
+import Mathlib.Tactic
+
+namespace alphalean
+
 theorem amc12a_2020_p10
   (n : ℕ)
   (h₀ : 0 < n)
-  (h₁ : real.logb 2 (real.logb 16 n) = real.logb 4 (real.logb 4 n)) :
-  (nat.digits 10 n).sum = 13 := by
+  (h₁ : Real.logb 2 (Real.logb 16 n) = Real.logb 4 (Real.logb 4 n)) :
+  (Nat.digits 10 n).sum = 13 := by
