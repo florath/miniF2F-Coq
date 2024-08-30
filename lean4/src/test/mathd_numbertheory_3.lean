84bafd3578c633afb336910963a747f6f072b1f5
Andreas Florath <andreas@florath.net>
2024-08-30 06:37:02 +0000

    Added import tactic
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_numbertheory_3.lean
+++ lean4/src/test/mathd_numbertheory_3.lean
@@ -1,6 +1,7 @@
 import Mathlib.Algebra.BigOperators.Intervals
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Nat.Defs
+import Mathlib.Tactic
 
 theorem mathd_numbertheory_3 :
   (∑ v0 in Finset.range 10, ((v0 + 1)^2)) % 10 = 5 := by
