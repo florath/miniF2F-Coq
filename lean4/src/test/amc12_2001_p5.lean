0f7b34b80c2f9e99c6bd2a3f30b5ca164796b0b5
Andreas Florath <andreas@florath.net>
2024-08-30 07:54:40 +0000

    Fixed amc12_2001_p5 for Lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/amc12_2001_p5.lean
+++ lean4/src/test/amc12_2001_p5.lean
@@ -1,2 +1,10 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Nat.Defs
+import Mathlib.Data.Nat.Factorial.Basic
+import Mathlib.Data.Nat.Factorial.BigOperators
+import Mathlib.Data.Nat.Factorial.Cast
+import Mathlib.Tactic
+
 theorem amc12_2001_p5 :
-  finset.prod (finset.filter (λ x, ¬ even x) (finset.range 10000)) (id : ℕ → ℕ) = (10000!) / ((2^5000) * 5000!) := by
+  Finset.prod (Finset.filter (λ x => ¬ Even x) (Finset.range 10000))
+  (id : ℕ → ℕ) = (Nat.factorial 10000) / ((2^5000) * (Nat.factorial 5000)) := by
