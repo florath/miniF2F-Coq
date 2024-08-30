14c9e460355b53d414380aba387d7fe2c0762137
Andreas Florath <andreas@florath.net>
2024-08-30 07:34:00 +0000

    Fixed aime_1983_p1 for Lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/aime_1983_p1.lean
+++ lean4/src/test/aime_1983_p1.lean
@@ -1,10 +1,12 @@
+import Mathlib.Data.Nat.Defs
+import Mathlib.Data.Real.Basic
 import Mathlib.Tactic
 
 theorem aime_1983_p1
   (x y z w : ℕ)
   (ht : 1 < x ∧ 1 < y ∧ 1 < z)
   (hw : 0 ≤ w)
-  (h0 : real.log w / real.log x = 24)
-  (h1 : real.log w / real.log y = 40)
-  (h2 : real.log w / real.log (x * y * z) = 12):
-  real.log w / real.log z = 60 := by
+  (h0 : Real.log w / Real.log x = 24)
+  (h1 : Real.log w / Real.log y = 40)
+  (h2 : Real.log w / Real.log (x * y * z) = 12):
+  Real.log w / Real.log z = 60 := by
