93e0ef7e3d1e792b3c46c4b4d293a5752abe94ba
Andreas Florath <andreas@florath.net>
2024-08-30 12:55:50 +0000

    Prepared algebra_sqineq_unitcircatbpabsamblt1 for lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/algebra_sqineq_unitcircatbpabsamblt1.lean
+++ lean4/src/test/algebra_sqineq_unitcircatbpabsamblt1.lean
@@ -1,5 +1,9 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
+
+namespace alphalean
+
 theorem algebra_sqineq_unitcircatbpabsamblt1
-  (a b: ℝ)
-  (h₀ : a^2 + b^2 = 1) :
-  a * b + ∥a - b∥ ≤ 1 := by
-  
+  (v0 v1: ℝ)
+  (hyp0 : v0^2 + v1^2 = 1) :
+  v0 * v1 + ( v0 - v1 ) ≤ 1 := by
