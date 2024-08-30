c0a399602336dc7addc7550751cd2b472757a990
Andreas Florath <andreas@florath.net>
2024-08-30 14:26:43 +0000

    Prepared imo_1960_p2 for lean4
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/imo_1960_p2.lean
+++ lean4/src/test/imo_1960_p2.lean
@@ -1,7 +1,12 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Nat.Defs
+import Mathlib.Tactic
+
+namespace alphalean
+
 theorem imo_1960_p2
-  (x : ℝ)
-  (h₀ : 0 ≤ 1 + 2 * x)
-  (h₁ : (1 - real.sqrt (1 + 2 * x))^2 ≠ 0)
-  (h₂ : (4 * x^2) / (1 - real.sqrt (1 + 2*x))^2 < 2*x + 9) :
-  -(1 / 2) ≤ x ∧ x < 45 / 8 := by
-  
+  (v0 : ℝ)
+  (hyp0 : 0 ≤ 1 + 2 * v0)
+  (hyp1 : (1 - Real.sqrt (1 + 2 * v0))^2 ≠ 0)
+  (hyp2 : (4 * v0^2) / (1 - Real.sqrt (1 + 2*v0))^2 < 2*v0 + 9) :
+  -(1 / 2) ≤ v0 ∧ v0 < 45 / 8 := by
