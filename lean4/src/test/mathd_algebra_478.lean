88be0f8d9d32ed8a5f036666182fbcd98e1d8da6
Andreas Florath <andreas@florath.net>
2024-08-30 06:12:27 +0000

    Removed unused variables
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_algebra_478.lean
+++ lean4/src/test/mathd_algebra_478.lean
@@ -2,7 +2,6 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Tactic
 
 theorem mathd_algebra_478
-  (b h v : ℝ)
   (v0 v1 v2 : ℝ)
   (hyp0 : 0 < v0 ∧ 0 < v1 ∧ 0 < v2)
   (hyp1 : v2 = 1 / 3 * (v0 * v1))
