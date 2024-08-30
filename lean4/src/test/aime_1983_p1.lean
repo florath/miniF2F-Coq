623c6e05bbc1337524c8cce750e8cad29d050eab
Andreas Florath <andreas@florath.net>
2024-08-30 07:37:18 +0000

    Unified variable and hypotheses nameing in aime_1983_p1
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/aime_1983_p1.lean
+++ lean4/src/test/aime_1983_p1.lean
@@ -3,10 +3,10 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Tactic
 
 theorem aime_1983_p1
-  (x y z w : ℕ)
-  (ht : 1 < x ∧ 1 < y ∧ 1 < z)
-  (hw : 0 ≤ w)
-  (h0 : Real.log w / Real.log x = 24)
-  (h1 : Real.log w / Real.log y = 40)
-  (h2 : Real.log w / Real.log (x * y * z) = 12):
-  Real.log w / Real.log z = 60 := by
+  (v0 v1 v2 v3 : ℕ)
+  (hyp0 : 1 < v0 ∧ 1 < v1 ∧ 1 < v2)
+  (hyp1 : 0 ≤ v3)
+  (hyp2 : Real.log v3 / Real.log v0 = 24)
+  (hyp3 : Real.log v3 / Real.log v1 = 40)
+  (hyp4 : Real.log v3 / Real.log (v0 * v1 * v2) = 12):
+  Real.log v3 / Real.log v2 = 60 := by
