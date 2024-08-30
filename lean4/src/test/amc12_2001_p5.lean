b9d56825bb45072182121a0b7efbf95d3c4f1377
Andreas Florath <andreas@florath.net>
2024-08-30 08:02:06 +0000

    Set recurson depth
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/amc12_2001_p5.lean
+++ lean4/src/test/amc12_2001_p5.lean
@@ -5,6 +5,8 @@ import Mathlib.Data.Nat.Factorial.BigOperators
 import Mathlib.Data.Nat.Factorial.Cast
 import Mathlib.Tactic
 
+set_option maxRecDepth 4096
+
 theorem amc12_2001_p5 :
   Finset.prod (Finset.filter (λ x => ¬ Even x) (Finset.range 10000))
   (id : ℕ → ℕ) = (Nat.factorial 10000) / ((2^5000) * (Nat.factorial 5000)) := by
