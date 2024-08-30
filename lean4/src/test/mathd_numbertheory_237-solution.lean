aa1c0c01c8a7f023a97873e631c71031029f0698
Andreas Florath <andreas@florath.net>
2024-08-30 10:55:38 +0000

    Added more proofs for mathd_numbertheory_237
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- lean4/src/test/mathd_numbertheory_237-solution.lean
+++ lean4/src/test/mathd_numbertheory_237-solution.lean
@@ -6,7 +6,25 @@ namespace alphalean
 
 -- version b9642c334f09c5f2ed1fc71e05fa9eef718f503e
 -- found 2024-08-30 12:52 duging model training
-
 theorem mathd_numbertheory_237 :
   (∑ k in (Finset.range 101), k) % 6 = 4 := by
 trivial
+
+-- version b9642c334f09c5f2ed1fc71e05fa9eef718f503e
+-- found 2024-08-30 12:52 duging model training
+theorem mathd_numbertheory_237_alt_00 :
+  (∑ k in (Finset.range 101), k) % 6 = 4 := by
+tauto
+
+-- version b9642c334f09c5f2ed1fc71e05fa9eef718f503e
+-- found 2024-08-30 12:52 duging model training
+theorem mathd_numbertheory_237_alt_01 :
+  (∑ k in (Finset.range 101), k) % 6 = 4 := by
+symm
+exact rfl
+
+-- version b9642c334f09c5f2ed1fc71e05fa9eef718f503e
+-- found 2024-08-30 12:52 duging model training
+theorem mathd_numbertheory_237_alt_02 :
+  (∑ k in (Finset.range 101), k) % 6 = 4 := by
+solve_by_elim
